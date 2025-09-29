bd341c7a83bc836aa3c9a437f5a4f932742f13cf
TripleBHomeBase <triplebhomebase@triplebvstation.lan>
2025-09-29 09:52:26 -0400

    Added and revised house code patterns for SLVR and SLVR SoCal

--- ott-slack.py
+++ ott-slack.py
@@ -92,16 +92,16 @@ CHANNEL_CONFIG = {
     "SLVR": {
         "spreadsheet_id": '1Vi6vr5lI41SM9yV4y0HVeq0tMreJmhMp4s1coVKPTHw',
         "linear_channel_id": 7260,
-        "house_code_pattern": r'(BOX\d+|EGH\d+|SLVR\d+|BOXFILL\d+|EGHFILL\d+|SLVRFILL\d+|SBAW\d+|CCA\d+|SGIHL\d+|SNHLR\d+|SNHLP\d+)',
-        "bumper_pattern": r'(BOXBUMP\d+|EGHBUMP\d+|SLVRBUMP\d+)',
+        "house_code_pattern": r'(EGH\d+|SLVR\d+|EGHFILL\d+|SLVRFILL\d+|SBAW\d+|CCA\d+|SGIHL\d+|SNHLR\d+|SNHLP\d+|FBLJK\d+|SETH\d+)',
+        "bumper_pattern": r'(EGHBUMP\d+|SLVRBUMP\d+)',
         "output_prefix": "SLVR",
         "processing_logic": "slvr"
     },
     "SLVR SoCal": {
         "spreadsheet_id": '1Vi6vr5lI41SM9yV4y0HVeq0tMreJmhMp4s1coVKPTHw',
         "linear_channel_id": 7790,
-        "house_code_pattern": r'(BOX\d+|EGH\d+|SLVR\d+|BOXFILL\d+|EGHFILL\d+|SLVRFILL\d+)',
-        "bumper_pattern": r'(BOXBUMP\d+|EGHBUMP\d+|SLVRBUMP\d+)',
+        "house_code_pattern": r'(EGH\d+|SLVR\d+|EGHFILL\d+|SLVRFILL\d+|SBAW\d+|CCA\d+|SGIHL\d+|SNHLR\d+|SNHLP\d+|FBLJK\d+|SETH\d+)',
+        "bumper_pattern": r'(EGHBUMP\d+|SLVRBUMP\d+)',
         "output_prefix": "SLVR_SOCAL",
         "processing_logic": "slvr socal"
     },
